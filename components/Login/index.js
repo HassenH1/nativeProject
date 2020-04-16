538eee23090a0321bf84b23c5b5423ac3952f4a9
HassenH1 <hasansaid51@gmail.com>
2020-04-16 14:25:14 -0700

    so far so good

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, TextInput, Dimensions, KeyboardAvoidingView, Platform } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Dimensions, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import { Button } from 'react-native-elements';
 
 
@@ -17,51 +17,57 @@ const Login = (props) => {
       behavior={Platform.OS == "ios" ? "padding" : "height"}
       style={styles.container}
     >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
 
-      <View style={styles.title}>
-        <Text style={{ fontSize: 49, fontWeight: "bold", textAlign: "left", textAlign: "center" }}>Login.</Text>
-        <View
-          style={{
-            borderBottomColor: '#89cff0',
-            borderBottomWidth: 1,
-            width: WIDTH / 2,
-            marginTop: 10
-          }}
-        />
-      </View>
+          <View style={styles.title}>
+            <Text style={{ fontSize: 49, fontWeight: "bold", textAlign: "left", textAlign: "center" }}>Login.</Text>
+            <View
+              style={{
+                borderBottomColor: '#89cff0',
+                borderBottomWidth: 1,
+                width: WIDTH / 2,
+                marginTop: 10
+              }}
+            />
+          </View>
 
-      <View>
-        <TextInput
-          placeholder="Email"
-          style={styles.input}
-          onChangeText={text => setInput({
-            ...input,
-            email: text
-          })}
-          value={input.email}
-        />
+          <View>
+            <TextInput
+              placeholder="Email"
+              style={styles.input}
+              onChangeText={text => setInput({
+                ...input,
+                email: text
+              })}
+              value={input.email}
+            />
 
-        <TextInput
-          placeholder="password"
-          style={styles.input}
-          onChangeText={text => setInput({
-            ...input,
-            password: text
-          })}
-          value={input.password}
-        />
+            <TextInput
+              placeholder="password"
+              style={styles.input}
+              onChangeText={text => setInput({
+                ...input,
+                password: text
+              })}
+              value={input.password}
+            />
 
-        <Button
-          title="Submit"
-          style={styles.btn}
-          onPress={() => console.log("clicked")}
-        />
-      </View>
+            <Button
+              title="Submit"
+              style={styles.btn}
+              onPress={() => console.log("clicked")}
+            />
+          </View>
 
-      <View style={{ marginTop: 18 }}>
-        <Text>Don't Have an Account? <Text onPress={() => props.navigation.navigate("sign up")}
-          style={{ color: "blue" }}>Signup</Text></Text>
-      </View>
+          <View style={{ marginTop: 18 }}>
+            <Text>Don't Have an Account? <Text onPress={() => props.navigation.navigate("sign up")}
+              style={{ color: "blue" }}>Signup</Text></Text>
+          </View>
+
+        </View>
+
+      </TouchableWithoutFeedback>
 
     </KeyboardAvoidingView>
   )
--- components/Signup/index.js
+++ components/Signup/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, TextInput, Dimensions, KeyboardAvoidingView, Platform } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Dimensions, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import { Button } from 'react-native-elements';
 
 
@@ -19,65 +19,68 @@ const Signup = (props) => {
       style={styles.container}
     >
 
-      <Text>Email: {input.email}</Text>
-      <Text>Username: {input.username}</Text>
-      <Text>Password: {input.password}</Text>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
 
-      <View style={styles.title}>
-        <Text style={{ fontSize: 49, fontWeight: "bold", textAlign: "left" }}>Register.</Text>
-        <View
-          style={{
-            borderBottomColor: '#89cff0',
-            borderBottomWidth: 1,
-            width: WIDTH /2,
-            marginTop: 10
-          }}
-        />
-      </View>
+          <View style={styles.title}>
+            <Text style={{ fontSize: 49, fontWeight: "bold", textAlign: "left" }}>Register.</Text>
+            <View
+              style={{
+                borderBottomColor: '#89cff0',
+                borderBottomWidth: 1,
+                width: WIDTH / 2,
+                marginTop: 10
+              }}
+            />
+          </View>
 
-      <View>
-        <TextInput
-          placeholder="Email"
-          style={styles.input}
-          onChangeText={text => setInput({
-            ...input,
-            email: text
-          })}
-          value={input.email}
-        />
+          <View>
+            <TextInput
+              placeholder="Email"
+              style={styles.input}
+              onChangeText={text => setInput({
+                ...input,
+                email: text
+              })}
+              value={input.email}
+            />
 
-        <TextInput
-          placeholder="username"
-          style={styles.input}
-          onChangeText={text => setInput({
-            ...input,
-            username: text
-          })}
-          value={input.username}
-        />
+            <TextInput
+              placeholder="username"
+              style={styles.input}
+              onChangeText={text => setInput({
+                ...input,
+                username: text
+              })}
+              value={input.username}
+            />
 
 
-        <TextInput
-          placeholder="password"
-          style={styles.input}
-          onChangeText={text => setInput({
-            ...input,
-            password: text
-          })}
-          value={input.password}
-        />
+            <TextInput
+              placeholder="password"
+              style={styles.input}
+              onChangeText={text => setInput({
+                ...input,
+                password: text
+              })}
+              value={input.password}
+            />
 
-        <Button
-          title="Submit"
-          style={styles.btn}
-          onPress={() => console.log("clicked")}
-        />
-      </View>
+            <Button
+              title="Submit"
+              style={styles.btn}
+              onPress={() => console.log("clicked")}
+            />
+          </View>
 
-      <View style={{ marginTop: 18 }}>
-        <Text>Already have an Account? <Text onPress={() => props.navigation.navigate("login")}
-          style={{ color: "blue" }}>Login</Text></Text>
-      </View>
+          <View style={{ marginTop: 18 }}>
+            <Text>Already have an Account? <Text onPress={() => props.navigation.navigate("login")}
+              style={{ color: "blue" }}>Login</Text></Text>
+          </View>
+
+        </View>
+
+      </TouchableWithoutFeedback>
 
     </KeyboardAvoidingView>
   )
@@ -91,7 +94,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    // justifyContent: "space-between"
   },
   title: {
     marginBottom: 50,
@@ -118,10 +120,4 @@ const styles = StyleSheet.create({
     color: "red",
     width: WIDTH - 30
   },
-  // testing: {
-  //   height: 40,
-  //   borderColor: "#000000",
-  //   borderBottomWidth: 1,
-  //   marginBottom: 36
-  // }
 })
