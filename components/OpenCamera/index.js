7c6572c1caa7270c19b377c8f880c36bd323d6cc
HassenH1 <hasansaid51@gmail.com>
2020-04-22 18:14:12 -0700

    photo description

--- components/OpenCamera/index.js
+++ components/OpenCamera/index.js
@@ -5,12 +5,17 @@ import * as ImagePicker from 'expo-image-picker';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { Button } from 'react-native-elements';
+import { Ionicons } from '@expo/vector-icons'
+import Constants from 'expo-constants'
+import { TextInput } from 'react-native-gesture-handler';
 
 
 const WIDTH = Dimensions.get("window").width
 const HEIGHT = Dimensions.get("window").height
 const OpenCamera = (props) => {
   const [img, setImg] = useState("")
+  const [name, setName] = useState("")
+  const [desc, setDesc] = useState("")
 
   const pickFromCamera = async () => {
     const { granted } = await Permissions.askAsync(Permissions.CAMERA)
@@ -75,29 +80,57 @@ const OpenCamera = (props) => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.heading}>
+        <Text style={{ fontWeight: "bold", fontSize: 40 }}>Post an Item</Text>
+      </View>
       {
         img !== ""
-          ? <View>
-              <Image
-                source={{ uri: img}}
-                style={styles.images}
+          ? <View style={{ flex: 1, justifyContent: "space-evenly", alignItems: "center" }}>
+            <View>
+              <Ionicons
+                name="ios-trash"
+                size={40}
+                style={styles.trash}
+                onPress={() => setImg("")}
               />
             </View>
+            <Image
+              source={{ uri: img }}
+              style={styles.images}
+            />
+            <View>
+              <TextInput
+                placeholder="Name"
+                style={styles.input}
+                onChangeText={text => setName(text)}
+                value={name}
+              />
+            </View>
+            <View>
+              <TextInput
+                multiline={true}
+                numberOfLines={4}
+                onChangeText={(text) => setDesc(text)}
+                value={desc}
+                style={styles.input}
+              />
+            </View>
+          </View>
           : <View>
-              <Button
-                title="Take Photo"
-                color="#f194ff"
-                onPress={() => pickFromCamera()}
-                style={styles.button}
-              />
+            <Button
+              title="Take Photo"
+              color="#f194ff"
+              onPress={() => pickFromCamera()}
+              style={styles.button}
+            />
 
-              <Button
-                title="Select Photo"
-                color="black"
-                onPress={() => pickFromGallery()}
-                style={styles.button}
-              />
-            </View>
+            <Button
+              title="Select Photo"
+              color="black"
+              onPress={() => pickFromGallery()}
+              style={styles.button}
+            />
+          </View>
       }
     </View>
   )
@@ -120,7 +153,34 @@ const styles = StyleSheet.create({
     width: WIDTH - 100,
     height: HEIGHT - 520,
     borderColor: 'black',
-    // borderWidth: 1,
-    // marginHorizontal: 3
+    zIndex: 1,
+    // position: "absolute"
+  },
+  trash: {
+    position: "absolute",
+    bottom: -310,
+    right: -18,
+    color: "red",
+    transform: [{ rotate: "30deg" }]
+  },
+  heading: {
+    alignItems: "center",
+    marginTop: Constants.statusBarHeight,
+  },
+  input: {
+    height: 60,
+    borderColor: 'whitesmoke',
+    borderWidth: 1,
+    width: WIDTH - 30,
+    borderRadius: 6,
+    shadowOffset: {
+      width: 0.2,
+      height: 0.2,
+    },
+    shadowColor: 'black',
+    shadowOpacity: 0.8,
+    padding: 10,
+    marginTop: 10,
+    fontSize: 15,
   },
 })
